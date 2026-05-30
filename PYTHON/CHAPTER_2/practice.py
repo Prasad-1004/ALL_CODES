b27b65339a22b86809a34e0d1afdd4b78df60a4a
Prasad-1004 <prasad.adhikary10@gmail.com>
2026-05-30 12:46:32 +0530

    new  commit

---
+++ PYTHON/CHAPTER_2/practice.py
@@ -0,0 +1,41 @@
+#Write a python program to add two numbers
+
+a = 10
+b = 20
+
+print(a+b)
+
+# Write a program to find remainder when a number is divided by z
+
+num = 15
+
+print("Remainder is",num % a )
+
+
+# Check the type of variable assigned using input() function
+
+am = input("Enter a value: ")
+
+print(type(am))
+
+
+
+# Use comparison operator to find out wheather a given variable is a greater than 'b' or not . Take a = 34 and b = 80
+
+
+aa = int(input("Enter First Number : "))
+bb = int(input("Enter Second Number : "))
+
+print("aa is greater than bb :", aa > bb)
+
+
+
+
+
+# Write a python program to find an average of two numbers entered by the user
+
+print("The average of two numbers is : ",(aa+bb)/2)
+
+# Write a python program to calculate the square of a number entered by the user 
+
+print(aa**2)
