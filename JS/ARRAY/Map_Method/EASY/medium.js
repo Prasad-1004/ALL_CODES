245b39be2c924f67e48927def89a2b16e4c963dc
Prasad-1004 <prasad.adhikary10@gmail.com>
2026-06-04 23:01:42 +0530

    second commit

--- JS/ARRAY/Map_Method/EASY/medium.js
+++ JS/ARRAY/Map_Method/EASY/medium.js
@@ -10,3 +10,36 @@ console.log(names.map(e=> `Mr. ${e}`));
 let roots = [4, 9, 16, 25]
 
 console.log(roots.map(e=> Math.sqrt(e)));
+
+
+
+// 3. Add index to each element
+// ["A", "B", "C"]
+
+// Output:
+
+// ["0-A", "1-B", "2-C"]
+
+
+
+let a = ["A", "B", "C"];
+
+let result = a.map((item, index) => `${index} ${item}`);
+
+console.log(result);
+
+
+// 4. Convert boolean to string
+// [true, false, true]
+
+
+let aa = [true, false, true]
+
+console.log(aa.map(e=> String(e)));
+
+
+// 5. Extract first letter of each name
+
+let naam = ["John", "Jane", "Alex"]
+
+console.log(naam.map(e=> e[0]));
